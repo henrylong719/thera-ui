5ce63b11b08fd109377866b4ac34cc526ea759cc
henrylong719 <henrylong719@gmail.com>
2022-11-14 07:56:05 +1030

    add unit test for submenu

--- src/components/menu/menu.test.tsx
+++ src/components/menu/menu.test.tsx
@@ -3,6 +3,7 @@ import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 
 import Menu, { MenuProps } from './menu';
 import MenuItem from './menuItem';
+import SubMenu from './subMenu';
 
 const testProps: MenuProps = {
   defaultIndex: '0',
@@ -21,8 +22,28 @@ const generateMenu = (props: JSX.IntrinsicAttributes & MenuProps) => {
       <MenuItem>active</MenuItem>
       <MenuItem disabled>disabled</MenuItem>
       <MenuItem>xxx</MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>drop1</MenuItem>
+        <MenuItem>drop2</MenuItem>
+      </SubMenu>
     </Menu>
   );
+};
+
+const createStyleFile = () => {
+  const cssFile: string = `
+    .thera-submenu {
+      display: none;
+    }
+    .thera-submenu.menu-opened{
+      display: block;
+    }
+  `;
+
+  const style = document.createElement('style');
+  // style.type = 'text/css';
+  style.innerHTML = cssFile;
+  return style;
 };
 
 let menuElement: HTMLElement,
@@ -31,8 +52,11 @@ let menuElement: HTMLElement,
 
 describe('test Menu and MenuItem component', () => {
   beforeEach(() => {
-    // eslint-disable-next-line testing-library/no-render-in-setup
-    render(generateMenu(testProps));
+    // eslint-disable-next-line testing-library/no-render-in-setup, testing-library/render-result-naming-convention
+    const wrapper = render(generateMenu(testProps));
+    // eslint-disable-next-line testing-library/no-container
+    wrapper.container.append(createStyleFile());
+
     menuElement = screen.getByTestId('test-menu');
     activeElement = screen.getByText('active');
     disabledElement = screen.getByText('disabled');
@@ -42,7 +66,8 @@ describe('test Menu and MenuItem component', () => {
     expect(menuElement).toBeInTheDocument();
     expect(menuElement).toHaveClass('thera-menu test');
     // eslint-disable-next-line testing-library/no-node-access
-    expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(menuElement.querySelectorAll(':scope > li').length).toEqual(4);
     expect(activeElement).toHaveClass('menu-item is-active');
     expect(disabledElement).toHaveClass('menu-item is-disabled');
   });
@@ -62,5 +87,9 @@ describe('test Menu and MenuItem component', () => {
     render(generateMenu(testVerProps));
     const menuElement = screen.getByTestId('test-menu');
     expect(menuElement).toHaveClass('menu-vertical');
+  });
+
+  it('should show dropdown items when hover on subMenu', () => {
+    expect(screen.queryByText('drop1')).not.toBeVisible();
   });
 });
