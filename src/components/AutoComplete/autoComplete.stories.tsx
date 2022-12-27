c589c9bb4c3ca3df189f226032659464c6b5820e
henrylong719 <henrylong719@gmail.com>
2022-12-27 12:41:55 +1030

    add autocomplete component

---
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {
+  AutoComplete,
+  AutoCompleteProps,
+  DataSourceType,
+} from './autoComplete';
+interface LakerPlayerProps {
+  value: string;
+  number: number;
+}
+interface GithubUserProps {
+  login: string;
+  url: string;
+  avatar_url: string;
+}
+export default {
+  title: 'AutoComplete 组件',
+  component: AutoComplete,
+  id: 'AutoComplete',
+  parameters: {
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+  },
+  // argTypes: { onClick: { action: 'clicked' }, onSelect: { action: 'selected' }, onChange: { action: 'changed' } },
+} as ComponentMeta<typeof AutoComplete>;
+
+// const Template: ComponentStory<typeof AutoComplete> = (args) => <AutoComplete {...args} />
+// export const Simple = Template.bind({})
+// const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins',
+// 'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
+// const handleFetch = (query: string) => {
+//   return lakers.filter(name => name.includes(query)).map(name => ({value: name}))
+// }
+// Simple.args = {
+//   fetchSuggestions: handleFetch,
+//   placeholder: "input players in Los Angeles lakers"
+// }
+export const ASimpleComplete: ComponentStory<typeof AutoComplete> = (args) => {
+  const lakers = [
+    'bradley',
+    'pope',
+    'caruso',
+    'cook',
+    'cousins',
+    'james',
+    'AD',
+    'green',
+    'howard',
+    'kuzma',
+    'McGee',
+    'rando',
+  ];
+  const handleFetch = (query: string) => {
+    return lakers
+      .filter((name) => name.includes(query))
+      .map((name) => ({ value: name }));
+  };
+  return (
+    <AutoComplete
+      {...args}
+      fetchSuggestions={handleFetch}
+      placeholder="input players in los Angers lakers"
+    />
+  );
+};
+ASimpleComplete.storyName = '1 basic search';
+
+export const BCustomComplete = (
+  args: JSX.IntrinsicAttributes & AutoCompleteProps
+) => {
+  const lakersWithNumber = [
+    { value: 'bradley', number: 11 },
+    { value: 'pope', number: 1 },
+    { value: 'caruso', number: 4 },
+    { value: 'cook', number: 2 },
+    { value: 'cousins', number: 15 },
+    { value: 'james', number: 23 },
+    { value: 'AD', number: 3 },
+    { value: 'green', number: 14 },
+    { value: 'howard', number: 39 },
+    { value: 'kuzma', number: 0 },
+  ];
+  const handleFetch = (query: string) => {
+    return lakersWithNumber.filter((player) => player.value.includes(query));
+  };
+  const renderOption = (item: DataSourceType) => {
+    const itemWithNumber = item as DataSourceType<LakerPlayerProps>;
+    return (
+      <>
+        <b>name: {itemWithNumber.value}</b>
+        <span>number: {itemWithNumber.number}</span>
+      </>
+    );
+  };
+  return (
+    <AutoComplete
+      {...args}
+      fetchSuggestions={handleFetch}
+      placeholder="input players in Los Angeles"
+      renderOption={renderOption}
+    />
+  );
+};
+BCustomComplete.storyName = '2 search';
+
+export const CAjaxComplete = (
+  args: JSX.IntrinsicAttributes & AutoCompleteProps
+) => {
+  const handleFetch = (query: string) => {
+    return fetch(`https://api.github.com/search/users?q=${query}`)
+      .then((res) => res.json())
+      .then(({ items }) => {
+        return items
+          .slice(0, 10)
+          .map((item: any) => ({ value: item.login, ...item }));
+      });
+  };
+
+  const renderOption = (item: DataSourceType) => {
+    const itemWithGithub = item as DataSourceType<GithubUserProps>;
+    return (
+      <>
+        <b>Name: {itemWithGithub.value}</b>
+        <span>url: {itemWithGithub.url}</span>
+      </>
+    );
+  };
+  return (
+    <AutoComplete
+      {...args}
+      fetchSuggestions={handleFetch}
+      placeholder="input github user names"
+      renderOption={renderOption}
+    />
+  );
+};
+CAjaxComplete.storyName = '3 support async search';
+
+// storiesOf('Nine：AutoComplete', module)
+//   .add('AutoComplete', simpleComplete, {info: {source: false, text: textComplete}})
+//   .add('auto select', customComplete,  {info: {source: false, text: textCustom}})
+//   .add('async search github users', ajaxComplete, {info: {source: false, text: textAjax}})
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,5 +1,19 @@
-import { FC, ReactElement } from 'react';
-import { InputProps } from '../Input/input';
+import classNames from 'classnames';
+import {
+  FC,
+  useState,
+  ChangeEvent,
+  KeyboardEvent,
+  ReactElement,
+  useEffect,
+  useRef,
+} from 'react';
+
+import { Transition } from 'react-transition-group';
+import useClickOutside from '../../hooks/useClickOutside';
+import useDebounce from '../../hooks/useDebounce';
+import Icon from '../Icon/icon';
+import Input, { InputProps } from '../Input/input';
 
 interface DataSourceObject {
   value: string;
@@ -8,12 +22,155 @@ interface DataSourceObject {
 export type DataSourceType<T = {}> = T & DataSourceObject;
 export interface AutoCompleteProps
   extends Omit<InputProps, 'onSelect' | 'onChange'> {
-  fetchSuggestion: (str: string) => DataSourceType[] | Promise<DataSourceType>;
+  fetchSuggestions: (
+    str: string
+  ) => DataSourceType[] | Promise<DataSourceType[]>;
   onSelect?: (Item: DataSourceType) => void;
   onChange?: (value: string) => void;
   renderOption?: (item: DataSourceType) => ReactElement;
 }
 
-const AutoComplete: FC<AutoCompleteProps> = (props) => {};
+export const AutoComplete: FC<AutoCompleteProps> = (props) => {
+  const {
+    fetchSuggestions,
+    onSelect,
+    onChange,
+    value,
+    renderOption,
+    ...restProps
+  } = props;
+
+  const [inputValue, setInputValue] = useState(value as string);
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [highlightIndex, setHighlightIndex] = useState(-1);
+  const triggerSearch = useRef(false);
+  const componentRef = useRef<HTMLDivElement>(null);
+  const debouncedValue = useDebounce(inputValue, 300);
+  useClickOutside(componentRef, () => {
+    setSuggestions([]);
+  });
+  useEffect(() => {
+    if (debouncedValue && triggerSearch.current) {
+      setSuggestions([]);
+      const results = fetchSuggestions(debouncedValue);
+      if (results instanceof Promise) {
+        setLoading(true);
+        results.then((data) => {
+          setLoading(false);
+          setSuggestions(data);
+          if (data.length > 0) {
+            setShowDropdown(true);
+          }
+        });
+      } else {
+        setSuggestions(results);
+        setShowDropdown(true);
+        if (results.length > 0) {
+          setShowDropdown(true);
+        }
+      }
+    } else {
+      setShowDropdown(false);
+    }
+    setHighlightIndex(-1);
+  }, [debouncedValue, fetchSuggestions]);
+  const highlight = (index: number) => {
+    if (index < 0) index = 0;
+    if (index >= suggestions.length) {
+      index = suggestions.length - 1;
+    }
+    setHighlightIndex(index);
+  };
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    switch (e.keyCode) {
+      case 13:
+        if (suggestions[highlightIndex]) {
+          handleSelect(suggestions[highlightIndex]);
+        }
+        break;
+      case 38:
+        highlight(highlightIndex - 1);
+        break;
+      case 40:
+        highlight(highlightIndex + 1);
+        break;
+      case 27:
+        setShowDropdown(false);
+        break;
+      default:
+        break;
+    }
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    console.log('triggered the value', value);
+    setInputValue(value);
+    if (onChange) {
+      onChange(value);
+    }
+    triggerSearch.current = true;
+  };
+  const handleSelect = (item: DataSourceType) => {
+    setInputValue(item.value);
+    setShowDropdown(false);
+    if (onSelect) {
+      onSelect(item);
+    }
+    triggerSearch.current = false;
+  };
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOption ? renderOption(item) : item.value;
+  };
+
+  const generateDropdown = () => {
+    return (
+      <Transition
+        in={showDropdown || loading}
+        animation="zoom-in-top"
+        timeout={300}
+        onExit={() => {
+          setSuggestions([]);
+        }}
+      >
+        <ul className="thera-suggestion-list">
+          {loading && (
+            <div className="suggestions-loading-icon">
+              <Icon icon="spinner" spin />
+            </div>
+          )}
+          {suggestions.map((item, index) => {
+            const cnames = classNames('suggestion-item', {
+              'is-active': index === highlightIndex,
+            });
+            return (
+              <li
+                key={index}
+                className={cnames}
+                onClick={() => handleSelect(item)}
+              >
+                {renderTemplate(item)}
+              </li>
+            );
+          })}
+        </ul>
+      </Transition>
+    );
+  };
+
+  return (
+    <div className="thera-auto-complete" ref={componentRef}>
+      <Input
+        {...restProps}
+        value={inputValue}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+
+      {generateDropdown()}
+    </div>
+  );
+};
 
 export default AutoComplete;
